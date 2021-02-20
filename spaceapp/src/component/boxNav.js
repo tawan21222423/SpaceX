bc0eec92d8e32d4c9f693bd7e51ef553b2fea018
thitiwut088 <thitiwut00897@gmail.com>
2021-02-20 22:56:20 +0700

    NavLink actionclassname

--- spaceapp/src/component/boxNav.js
+++ spaceapp/src/component/boxNav.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import "../styles/component.css"
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const BoxNav = ({text, path}) => {
     return (
-        <Link className="Nav" to={path} activeClassName="active">
+        <NavLink className="Nav" to={path} activeClassName="active">
             <p>{text}</p>
-        </Link>
+        </NavLink>
     )
 }
 
